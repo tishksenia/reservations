3451f64d991861dc458b8bf78e737960ca837a62
tishksenia <tishksenia@yandex.ru>
2019-05-15 15:11:02 +0300

    working on Day module: initial markup, basic timetable, position of timestamp

--- src/js/Day.js
+++ src/js/Day.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class Day extends React.Component {
-  state = {};
+  state = { times: [], currentTime: "", timestampStyle: "" };
   componentDidMount() {
     this.setState({
       currentTime: this.getCurrentTime(),
@@ -12,7 +12,7 @@ class Day extends React.Component {
     var times = [];
     for (var i = 0; i <= 23; i++) {
       times.push(i + ":00");
-      times.push(i + ":30");
+      //times.push(i + ":30");
     }
     return times;
   }
@@ -21,15 +21,36 @@ class Day extends React.Component {
     var currentTime =
       today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     //{currentTime: currentTime} -> {currentTime}
-    //this.setState({ currentTime });
     return currentTime;
   }
+  getTimestampStyle() {
+    // will always be HH:MM:SS
+    const block_height = 50;
+    var time_array = this.state.currentTime.split(":");
+    var hours = time_array[0];
+    var mins = time_array[1];
+    var styles = {
+      top: hours * 50 + "px"
+    };
+    return styles;
+  }
+
   render() {
     return (
       <div className="container">
         <p>{this.state.currentTime}</p>
-
-        <p>{this.state.times}</p>
+        <div className="time-table">
+          <div className="times">
+            {this.state.times.map(value => (
+              <p className="times__item" key={value}>
+                {value}
+              </p>
+            ))}
+          </div>
+          <div className="table">
+            <div className="timestamp" style={this.getTimestampStyle()} />
+          </div>
+        </div>
       </div>
     );
   }
