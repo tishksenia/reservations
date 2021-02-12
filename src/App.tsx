2ec32152042de0bff018a24ca8a53eca92ca8069
Ksenia Tishchenko <tishksenia@yandex.ru>
2021-02-12 14:31:14 +0300

    add typescript, rename and reogranize files, fix workflow scripts

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TimeTable from "./TimeTable";
 import Day from "./Day";
-import Header from "./layout/Header";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {/* <Header /> */}
         <TimeTable start="00:00" end="23:00" />
       </div>
     );
